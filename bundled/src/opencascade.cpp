e97dd4fdcf4a4c1c23992ff5a3c18ecdfd7da482
Jukka Aho <ahojukka5@gmail.com>
2026-07-02 08:05:22 +0300

    build: register occ_gltf module

--- bundled/src/opencascade.cpp
+++ bundled/src/opencascade.cpp
@@ -35,6 +35,7 @@ void register_occ_history(jlcxx::Module&);
 void register_occ_bopalgo_analyzer(jlcxx::Module&);
 void register_occ_triangulation(jlcxx::Module&);
 void register_occ_interface_static(jlcxx::Module&);
+void register_occ_gltf(jlcxx::Module&);
 
 JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
 {
@@ -71,4 +72,5 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
   register_occ_bopalgo_analyzer(mod);
   register_occ_triangulation(mod);
   register_occ_interface_static(mod);
+  register_occ_gltf(mod);
 }
